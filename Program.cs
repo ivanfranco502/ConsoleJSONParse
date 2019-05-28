9ad541aa23511ddf2448f682ae0b160680b53c3d
Ivan Franco <ifranco@tupaca.com>
2019-05-27 22:42:00 -0300

    second updated

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace ConsoleJSONParse
 {
@@ -6,7 +9,37 @@ namespace ConsoleJSONParse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string jsonData = ReadFile();
+            var stockModels = JsonConvert.DeserializeObject<List<StockModel>>(jsonData);
+
+            Console.WriteLine($"Stock models {stockModels.Count}");
         }
+
+        public static string ReadFile(){
+            using (StreamReader r = new StreamReader("models.json"))
+            {
+                return r.ReadToEnd();
+            }
+        }
+    }
+
+    public class StockModel{
+        public string manufacter { get; set; }
+        public string model { get; set; }
+        public string partNumber { get; set; }
+        public string uaNumber { get; set; }
+        public string serialNumber { get; set; }
+        public string condition { get; set; }
+        public string inShippingRef { get; set; }
+        public string inShippingDate { get; set; }
+        public string inLocation { get; set; }
+        public string outShippingRef { get; set; }
+        public string outShippingDate { get; set; }
+        public string outLocation { get; set; }
+        public string feeds { get; set; }
+        public string localShippingRef { get; set; }
+        public string localShippingDate { get; set; }
+        public string cableOperator;
+
     }
 }
