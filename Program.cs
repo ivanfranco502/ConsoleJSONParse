95cc646779a238ac1b2eef2073452c47d322a2e1
Ivan Franco <ifranco@tupaca.com>
2019-06-04 12:14:03 -0300

    extract stockmodel to new class

--- Program.cs
+++ Program.cs
@@ -23,23 +23,4 @@ namespace ConsoleJSONParse
         }
     }
 
-    public class StockModel{
-        public string manufacter { get; set; }
-        public string model { get; set; }
-        public string partNumber { get; set; }
-        public string uaNumber { get; set; }
-        public string serialNumber { get; set; }
-        public string condition { get; set; }
-        public string inShippingRef { get; set; }
-        public string inShippingDate { get; set; }
-        public string inLocation { get; set; }
-        public string outShippingRef { get; set; }
-        public string outShippingDate { get; set; }
-        public string outLocation { get; set; }
-        public string feeds { get; set; }
-        public string localShippingRef { get; set; }
-        public string localShippingDate { get; set; }
-        public string cableOperator;
-
-    }
 }
